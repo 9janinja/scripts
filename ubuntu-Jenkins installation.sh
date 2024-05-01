cb5666023e4591bd33b87aeaafb1dddabdcdd5c1
9janinja <awure_barry@yahoo.com>
2024-05-01 02:31:48 +0100

    Update ubuntu-Jenkins installation.sh

--- ubuntu-Jenkins installation.sh
+++ ubuntu-Jenkins installation.sh
@@ -1,7 +1,7 @@
 # Jenkins Installation And Setup In AWS EC2 Ubuntu Instance
 
 # Update the repositories
-sudo apt update
+sudo apt update -y
 
 # Install Java
 sudo apt install openjdk-11-jdk
@@ -16,7 +16,7 @@ wget -q -O - https://pkg.jenkins.io/pubkey | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/ubuntu-bionic binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list'
 
 # Update package lists again
-sudo apt update
+sudo apt update -y
 
 # Install Jenkins
 sudo apt install jenkins
